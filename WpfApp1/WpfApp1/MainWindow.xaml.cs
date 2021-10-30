4402f39d246b2090b1fc36338fb1ef422b143cc8
GeogeKhodaev <74020503+GeogeKhodaev@users.noreply.github.com>
2021-10-30 13:48:13 +0300

    Add files via upload

---
+++ WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                double a, b, c, d;
+                a = Convert.ToDouble(aTextBox.Text);
+                b = Convert.ToDouble(bTextBox.Text);
+                c = Convert.ToDouble(cTextBox.Text);
+                d = b * b - 4 * a * c;
+                if (b * b - 4 * a * c < 0 || a == 0)
+                {
+                    MessageBox.Show("Решений нет");
+                    returnTextbox.Text = " ";
+                    returnTextbox2.Text = " ";
+                }
+                else if (b * b - 4 * a * c == 0)
+                {
+                    returnTextbox.Text = Convert.ToString((-b + Math.Sqrt(b * b - 4 * a * c)) / (2 * a));
+                    returnTextbox2.Text = " ";
+                }
+                else
+                {
+                    returnTextbox.Text = Convert.ToString((-b + Math.Sqrt(b * b - 4 * a * c)) / (2 * a));
+                    returnTextbox2.Text = Convert.ToString((-b - Math.Sqrt(b * b - 4 * a * c)) / (2 * a));
+                }
+            }
+            catch (FormatException) 
+            {
+                MessageBox.Show("заполните поля корректными данными");
+            }
+        }
+    }
+}
---
+++ WpfApp2/WpfApp2/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                double a, b, c;
+                a = Convert.ToDouble(aTextbox.Text);
+                b = Convert.ToDouble(bTextbox.Text);
+                c = Convert.ToDouble(cTextbox.Text);             
+                if (a == b && a != c || a == c && a != b || b == c && b != a)
+                    returnTextbox.Text = "треугольник равнобедренный";
+                if (a == b && b == c)
+                    returnTextbox.Text = "треугольник равносторонний";
+                if (a != b && a != c && b != c)
+                    returnTextbox.Text = "треугольник разностонний";
+                if (Math.Sqrt(a * a + b * b) == Math.Sqrt(c * c))
+                    returnTextbox.Text = "треугольник прямоугольный";
+                if (a <= 0 || b <= 0 || c <= 0)
+                {
+                    MessageBox.Show("такого треугольника не существует");
+                    returnTextbox.Text = " ";
+                }
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Введите корректные данные");
+            }
+
+        }
+    }
+}
